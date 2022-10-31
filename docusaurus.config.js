09dc77398cc237fda0427cf93453e178030d5dc6
inspiringsource <ab.b2k3@gmail.com>
2022-10-31 19:43:40 +0100

    fixed Architecture Diagram

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
             position: 'left',
             label: 'Review',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/inspiringsource/aws-solutions-architect',
             label: 'GitHub',
